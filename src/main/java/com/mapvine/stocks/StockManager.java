3356660b926ba09fa10737d178a1d8c05521b928
Crystal <omaracrystal@gmail.com>
2016-08-02 13:24:27 -0600

    9 out of 10 tests completed

--- src/main/java/com/mapvine/stocks/StockManager.java
+++ src/main/java/com/mapvine/stocks/StockManager.java
@@ -6,15 +6,38 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+/* I am adding */
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.IllegalFormatCodePointException;
+import java.util.Map;
 
 public class StockManager {
 
+    List<Stock> results = new ArrayList<Stock>();
+    String[] str=new String[100];
+    int[] shr=new int[100];
+    BigDecimal[] prc=new BigDecimal[100];
+    int i=0,j=0;
+    int share=0;
+    double total=0;
     /**
      * Get the list of stocks currently under management by its ticker symbol.
      */
     public List<Stock> findByTicker(final String ticker) {
         // TODO: Implement me.
-        return null;
+        List<Stock> r = new ArrayList<Stock>();
+        for (Stock listItem : results) {
+            if(listItem.getTicker().equalsIgnoreCase(ticker))
+            {
+                r.add(listItem);
+            }
+        }
+        return r;
     }
 
     /**
@@ -22,7 +45,23 @@ public class StockManager {
      */
     public BigDecimal getValueUnderManagerByTicker(final String ticker) {
         // TODO: Implement me.
-        return null;
+        total=0;
+        for(int i=0;i<j;i++) {
+            if (str[i].equals(ticker.toLowerCase())) {
+                total = total + prc[i].doubleValue() * shr[i];
+            }
+        }
+        if(total==0)
+        {
+            BigDecimal bg=new BigDecimal(total);
+            total=0;
+            return bg;
+        }
+        else {
+            BigDecimal bg = new BigDecimal(total).setScale(4, RoundingMode.HALF_EVEN);
+            total = 0;
+            return bg;
+        }
     }
 
     /**
@@ -30,7 +69,13 @@ public class StockManager {
      */
     public int numberOfStocksByTicker(final String ticker) {
         // TODO: Implement me.
-        return -1;
+        share=0;
+        for(int i=0;i<j;i++) {
+            if (str[i].equals(ticker.toLowerCase())) {
+                share=share+shr[i];
+            }
+        }
+        return share;
     }
 
     /**
@@ -42,6 +87,20 @@ public class StockManager {
      */
     public void buyStock(final Stock stock, final int numberOfShares) {
         // TODO: Implement me.
+        if(numberOfShares<=0)
+        {
+            throw new IllegalArgumentException("No of share is less than or equal to 0");
+        }
+        else
+        {
+            results.add(stock);
+            String stk=stock.getTicker().toLowerCase();
+            BigDecimal price=stock.getPrice();
+            str[j]=stk;
+            shr[j]=numberOfShares;
+            prc[j]=price;
+            j++;
+        }
     }
 
     /**
@@ -57,7 +116,71 @@ public class StockManager {
      */
     public Optional<Set<BigDecimal>> sellStock(final String ticker, final int numberOfShares, final BigDecimal sharePrice) {
         // TODO: Implement me.
-        return null;
+        if(numberOfShares<=0)
+        {
+            throw new IllegalArgumentException("No of share is less than or eqaul to zero");
+        }
+        else {
+            share = 0;
+            double price = 0;
+            int chk=0,chk1=0;
+            for (int i = 0; i < j; i++) {
+                if (str[i].equals(ticker.toLowerCase())) {
+                    share = share + shr[i];
+                    price = price + prc[i].doubleValue();
+                    chk++;
+                    if(chk==2)
+                    {
+                        if(shr[i]>shr[i-1])
+                        {
+                            if(numberOfShares<shr[i-1])
+                            {
+                                int b;
+                                b=shr[i-1]-numberOfShares;
+                                shr[i-1]=b;
+                            }
+                            else if(numberOfShares>shr[i-1])
+                            {
+                                int b;
+                                b=numberOfShares-shr[i-1];
+                                shr[i-1]=b;
+                                if(b!=0)
+                                {
+                                    shr[i]=shr[i]-b;
+                                }
+                            }
+                        }
+                        else if(shr[i]<shr[i-1])
+                        {
+                            if(numberOfShares<shr[i])
+                            {
+                                int b;
+                                b=shr[i]-numberOfShares;
+                                shr[i]=b;
+                            }
+                            else if(numberOfShares>shr[i])
+                            {
+                                int b;
+                                b=numberOfShares-shr[i];
+                                shr[i]=b;
+                                if(b!=0)
+                                {
+                                    shr[i-1]=shr[i-1]-b;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // share = share - numberOfShares;
+            double pr = price - sharePrice.doubleValue();
+            BigDecimal big = new BigDecimal(share);
+            Set<BigDecimal> set = new HashSet<BigDecimal>();
+            set.add(big);
+
+            Optional<Set<BigDecimal>> s = Optional.of(set);
+            return s;
+        }
     }
 
     /**
